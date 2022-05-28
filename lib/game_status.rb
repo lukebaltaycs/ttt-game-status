dff2885bd750f2db4a3f097bcca85be406b34181
Luke Baltay <luke.baltay@yale.edu>
2022-05-28 15:51:18 -0400

    commit tree aac2395442571470a5806ee48f7f8697f9f460e7

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,45 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+
+WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
+
+def won?(board)
+  WIN_COMBINATIONS.each do |combination|
+    if board[combination[0]] == board[combination[1]] && board[combination[1]] == board[combination[2]] && position_taken?(board, combination[2]) == true
+    return combination
+    end
+  end
+  nil
+end
+    
+def full?(board)
+  all_filled = board.all? do |spot|
+    spot == "X" || spot == "O"
+  end
+end
+
+def draw?(board)
+  if full?(board) == true && won?(board) == nil
+    return true
+  else
+    false
+  end
+end
+
+def over?(board)
+  if draw?(board) == true || won?(board) != nil
+    return true
+  else
+    false
+  end
+end
+
+def winner(board)
+  if won?(board) != nil
+    combination = won?(board)
+    return board[combination[0]]
+  else
+    nil
+  end
+ end
